c818a01b756ac484fc345ec04f2b606dcd982ab9
Vipin Mishra <52159805+thevipinmishra@users.noreply.github.com>
2020-01-01 09:50:45 +0530

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,17 +4,24 @@ const colors = document.querySelectorAll(".colors__grid .color");
 colors.forEach(color => {
   color.style.background = color.dataset.color;
   color.innerHTML = `<span>Copy</span>`;
-  color.setAttribute("role", "button");
-  color.setAttribute("tabindex", "0");
   color.addEventListener("click", function() {
     const alert = document.querySelector(".alert");
     alert.style.background = color.dataset.color;
     alert.classList.toggle("flash");
     setTimeout(function() {
       alert.classList.remove("flash");
-    }, 700);
+    }, 500);
   });
 });
+
+// Color Count
+const flatColors = document.querySelectorAll(".flat__colors .color");
+const flatCount = flatColors.length;
+document.querySelector("#flat-count").textContent = `${flatCount} Colors`;
+
+const matColors = document.querySelectorAll(".material__colors .color");
+const matCount = matColors.length;
+document.querySelector("#material-count").textContent = `${matCount} Colors`;
 
 // Copy to Clipbaord
 new ClipboardJS(".color", {
